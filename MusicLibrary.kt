451397ac42dd01633d95f4da099dfc55021f3898
aans85889-droid <aans85889@gmail.com>
2026-08-04 22:22:43 +0300

    Delete MusicLibrary.kt

--- MusicLibrary.kt
+++
@@ -1,6 +0,0 @@
-package com.example.music
-
-object MusicLibrary {
-    // علامة (+) في الكوتلن تقوم بدمج القوائم تلقائياً وبسرعة فائقة في قائمة واحدة كبرى!
-    val allSheelahs = AlfarwanLibrary.songs + GhreebLibrary.songs + AbdullahLibrary.songs
-}
